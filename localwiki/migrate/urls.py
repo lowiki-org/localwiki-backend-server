bb7959a7ae97d273db5215759f5c58fa88e324a1
Superbil <superbil@gmail.com>
2016-10-26 03:30:23 +0800

    Fix bug of wrong url on import

--- localwiki/migrate/urls.py
+++ localwiki/migrate/urls.py
@@ -5,5 +5,5 @@ from migrate.views import RegionPageMigrateView, RegionPageExportView
 
 urlpatterns = patterns('',
     url(r'^(?P<region>[^/]+?)/_import/?$', RegionPageMigrateView.as_view(), name="regions"),
-    url(r'^(?P<region>[^/]+?)/_export/?$', RegionPageExportView.as_view(), name="regions"),
+    url(r'^(?P<region>[^/]+?)/_export/?$', RegionPageExportView.as_view(), name="export"),
 )
