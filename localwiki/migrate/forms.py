00e89ff9313d0dd682373f2b4a7d24bbae76a30c
Pomin Wu <pomin5@gmail.com>
2016-11-24 07:16:20 +0800

    Fixed export problems
    - Problems in exporting TXT, CSV, KML are fixed.

--- localwiki/migrate/forms.py
+++ localwiki/migrate/forms.py
@@ -4,12 +4,16 @@ from django.utils.translation import ugettext_lazy
 
 from .models import PageMigrateSource, PageExport
 
-EXPORT_TYPE_CHOICES = (('csv', 'csv'),
-                       ('json', 'json'),
-                       ('txt', 'txt'))
+EXPORT_TYPE_CHOICES = (
+    ('csv', 'CSV'),
+    # ('json', 'JSON'),
+    ('txt', 'TXT'),
+    ('kml', 'KML'),
+)
 
 
 class PageMigrateSourceForm(forms.ModelForm):
+
     class Meta:
         model = PageMigrateSource
         fields = ('source_file', 'tags')
@@ -23,7 +27,8 @@ class PageMigrateSourceForm(forms.ModelForm):
 
 
 class PageExportSourceForm(forms.Form):
-    export_type = forms.ChoiceField(widget=forms.Select, choices=EXPORT_TYPE_CHOICES)
+    export_type = forms.ChoiceField(
+        widget=forms.Select, choices=EXPORT_TYPE_CHOICES)
 
     def __init__(self, *args, **kwargs):
         region = kwargs.pop('region', None)
--- localwiki/migrate/views.py
+++ localwiki/migrate/views.py
@@ -2,7 +2,6 @@
 from django.views.generic import TemplateView
 from django.contrib import messages
 from django.utils.translation import ugettext as _
-from django.utils.encoding import smart_str
 from django.http import HttpResponse
 from localwiki.utils.urlresolvers import reverse
 
@@ -15,6 +14,12 @@ from .forms import PageMigrateSourceForm, PageExportSourceForm
 from .unicode_csv import UnicodeDictReader, UnicodeWriter
 
 from django.contrib.gis.geos import GEOSGeometry
+
+from zipfile import ZipFile
+from fastkml import kml
+from pygeoif import geometry
+from html2text import html2text
+
 
 class RegionPageMigrateView(RegionAdminRequired, RegionMixin, FormView):
     template_name = 'migrate/region_page_import.html'
@@ -45,7 +50,8 @@ class RegionPageMigrateView(RegionAdminRequired, RegionMixin, FormView):
             migrator.add_record(rec)
         migrator.migrate()
 
-        messages.add_message(self.request, messages.SUCCESS, _(u"File has been imported.  Total %(total)d records updated, including %(new)d new records.") % migrator.migration_result)
+        messages.add_message(self.request, messages.SUCCESS, _(
+            u"File has been imported.  Total %(total)d records updated, including %(new)d new records.") % migrator.migration_result)
 
         return super(RegionPageMigrateView, self).form_valid(form)
 
@@ -63,26 +69,39 @@ class RegionPageExportView(RegionAdminRequired, RegionMixin, FormView):
         return reverse('migrate:regions', kwargs={'region': self.get_region().slug})
 
     def form_valid(self, form):
-        rows = []
-        rows.append(['url', 'name', 'slug', 'tags', 'region', 'content'])
+        exporter = RegionExporter(
+            region=self.get_region(),
+            export_type=form['export_type'].value())
+        exporter.export_to_file()
 
-        export_file_name = '/tmp/export_file.csv'
-        with open(export_file_name, 'w') as f:
-            writer = UnicodeWriter(f)
-            writer.writerows(rows)
+        messages.add_message(
+            self.request, messages.SUCCESS, _(u"Export success."))
 
-        messages.add_message(self.request, messages.SUCCESS, _(u"Export success."))
+        if exporter.file_mime_type == 'text/csv':
+            ext = 'csv'
+        elif exporter.file_mime_type == 'application/json':
+            ext = 'json'
+        elif exporter.file_mime_type == 'application/vnd.google-earth.kml+xml':
+            ext = 'kml'
+        elif exporter.file_mime_type == 'text/plain':
+            ext = 'txt'
+        elif exporter.file_mime_type == 'application/zip':
+            ext = 'zip'
 
         # Send file to user
-        response = HttpResponse(mimetype='application/force-download')
-        response['Content-Disposition'] = 'attachment; filename=%s' % smart_str("export_csv.csv")
-        response['X-Sendfile'] = smart_str(export_file_name)
+        # response = HttpResponse(mimetype='application/force-download')
+        response = HttpResponse(
+            exporter.file, content_type=exporter.file_mime_type)
+        response[
+            'Content-Disposition'] = 'attachment; filename=export.%s' % ext
+        response['X-Sendfile'] = 'export.%s' % ext
 
         # return super(RegionPageExportView, self).form_valid(form)
         return response
 
 
 import mwparserfromhell
+
 
 class PageTemplateMigrator():
 
@@ -92,7 +111,7 @@ class PageTemplateMigrator():
         self.param_keys = []
         self.template_page = None
         self.tags = []
-        self.migration_result = { 'total': 0, 'new': 0 }
+        self.migration_result = {'total': 0, 'new': 0}
 
     def add_record(self, record):
         for k, v in record.iteritems():
@@ -101,13 +120,14 @@ class PageTemplateMigrator():
 
     def get_templates(self):
         return Page.objects.filter(
-                slug__startswith='templates/', region=self.region
-            )
+            slug__startswith='templates/', region=self.region
+        )
 
     def set_template(self, page):
         self.template_page = page
         self.template_name = page.name.replace('Templates/', '')
-        self.param_keys = [ unicode(t.name) for t in mwparserfromhell.parse(page.content).filter_templates() ]
+        self.param_keys = [unicode(t.name) for t in mwparserfromhell.parse(
+            page.content).filter_templates()]
 
     def add_tag(self, tag):
         self.tags.append(tag)
@@ -155,11 +175,12 @@ class PageTemplateMigrator():
 
         if self.template_page != None and len(self.param_keys) > 0:
             new_page.content = u'{{%s}}' % "|".join(
-                    [self.template_name] + [ u'%s=%s' % (k, record[k]) for k in self.param_keys if k in record ])
+                [self.template_name] + [u'%s=%s' % (k, record[k]) for k in self.param_keys if k in record])
         new_page.save()
 
         if not PageScore.objects.filter(page=new_page).exists():
-            score = PageScore(score=1, page=new_page, page_content_length=len(new_page.content)).save()
+            score = PageScore(score=1, page=new_page,
+                              page_content_length=len(new_page.content)).save()
 
         self.migration_result['total'] = self.migration_result['total'] + 1
 
@@ -178,5 +199,89 @@ class PageTemplateMigrator():
                 mapdata = new_page.mapdata
             except MapData.DoesNotExist:
                 mapdata = MapData(page=new_page, region=self.region)
-            mapdata.points = GEOSGeometry("""MULTIPOINT (%s %s)""" % (longitude, latitude))
+            mapdata.points = GEOSGeometry(
+                """MULTIPOINT (%s %s)""" % (longitude, latitude))
             mapdata.save()
+
+
+class RegionExporter:
+
+    def __init__(self, **kwargs):
+        self.region = kwargs['region']
+        self.export_type = kwargs['export_type']
+        self.file = None
+        self.file_mime_type = None
+
+    def _get_export_pages(self):
+        return self.region.page_set.exclude(slug__startswith='template')
+
+    def export_to_file(self):
+        if self.export_type == 'csv':
+            self.exportCSV()
+        elif self.export_type == 'json':
+            self.exportJSON()
+        elif self.export_type == 'txt':
+            self.exportTXT()
+        elif self.export_type == 'kml':
+            self.exportKML()
+
+    def exportTXT(self):
+        for page in self._get_export_pages():
+            with open('/tmp/%s.txt' % page.id, 'w') as f:
+                f.write('## ')
+                f.write(page.name.encode('utf-8'))
+                f.write('\n\n')
+                f.write(html2text(page.content).encode('utf-8'))
+                f.close()
+        with ZipFile('/tmp/export.zip', 'w') as zf:
+            for page in self._get_export_pages():
+                zf.write('/tmp/%s.txt' % page.id)
+            zf.close()
+        self.file = open('/tmp/export.zip', 'r')
+        self.file_mime_type = 'application/zip'
+
+    def exportJSON(self):
+        pass
+
+    def exportCSV(self):
+        rows = []
+        rows.append(['id', 'name', 'slug', 'tags',
+                     'region', 'region_name', 'content'])
+        for page in self._get_export_pages():
+            rows.append([
+                str(page.id),
+                page.name,
+                page.slug,
+                " ".join([t.name for t in page.pagetagset.tags.all()]),
+                self.region.slug,
+                self.region.full_name,
+                page.content])
+
+        self.filename = '/tmp/export_file.csv'
+        with open(self.filename, 'w') as f:
+            writer = UnicodeWriter(f)
+            writer.writerows(rows)
+        self.file = open(self.filename, 'r')
+        self.file_mime_type = 'text/csv'
+
+    def exportKML(self):
+        k = kml.KML()
+        ns = '{http://www.opengis.net/kml/2.2}'
+        d = kml.Document(ns, '0', 'Export', 'Exported from Lowiki')
+        k.append(d)
+        f = kml.Folder(ns, '1', 'Locations', 'Locations in Lowiki')
+        d.append(f)
+        for page in self._get_export_pages():
+            try:
+                p = kml.Placemark(ns, str(page.id),
+                                  page.name, '')
+                p.geometry = geometry.from_wkt(page.mapdata.geom.wkt)
+                f.append(p)
+            except:
+                pass
+        filename = '/tmp/export.kml'
+        with open(filename, 'w') as f:
+            f.write(k.to_string().encode('utf-8'))
+            f.close()
+        self.file = open(filename, 'r')
+        self.file_mime_type = 'application/vnd.google-earth.kml+xml'
